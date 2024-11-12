da6abc1fc9d8e8bdd1f4faf655008784e4b60bdb
AlejoZA <117043041+AlejoZA@users.noreply.github.com>
2024-11-12 00:32:42 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,39 +1,23 @@
 from sqlalchemy import create_engine
 import psycopg2
 
-##SI no funciona el import intentar con la funcion comentada y eliminar la otra funcion
-
 # Función para conectar a las bases de datos de origen y destino usando SQLAlchemy
-'''def connect_databases():
-    # Conexión a la base de datos de origen (por ejemplo, PostgreSQL)
-    db_op = create_engine(
-        'postgresql+psycopg2://Dani:invitado@localhost/rapidosyfuriosos'
-    )
-
-    # Conexión a la base de datos de destino
-    db_etl = create_engine(
-        'postgresql+psycopg2://Dani:invitado@localhost/etl'
-    )
-
-    return db_op, db_etl
-'''
-
 def connect_databases():
-    # Conexión a la base de datos de origen (por ejemplo, PostgreSQL)
-    db_op = psycopg2.connect(
-        host="localhost",
-        database="rapidosyfuriosos",  # Nombre de la base de datos de origen
-        user="Dani",             # Usuario de la base de datos de origen
-        password="invitado"      # Contraseña del usuario
-    )
+    try:
+        # Conexión a la base de datos de origen
+        db_op = create_engine(
+            'postgresql+psycopg2://alejandro:Alejo1193@localhost/rapidosyfuriosos'
+        )
+        print("Conexión a la base de datos 'rapidosyfuriosos' exitosa.")
 
-    # Conexión a la base de datos de destino (puede ser otra base de datos, dependiendo de tu configuración)
-    db_etl = psycopg2.connect(
-        host="localhost",
-        database="etl",  # Nombre de la base de datos de destino
-        user="Dani", # Usuario de la base de datos de destino
-        password="invitado"  # Contraseña del usuario
-    )
+        # Conexión a la base de datos de destino
+        db_etl = create_engine(
+            'postgresql+psycopg2://alejandro:Alejo1193@localhost/etl'
+        )
+        print("Conexión a la base de datos 'etl' exitosa.")
+    
+    except Exception as e:
+        print(f"Error en la conexión: {e}")
+        db_op, db_etl = None, None
 
     return db_op, db_etl
-
